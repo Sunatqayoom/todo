d01826878df76769e562bed82a8bf543172e0d12
Sunatqayoom <115891537+Sunatqayoom@users.noreply.github.com>
2025-05-16 16:07:27 +0530

    update

--- src/main/java/com/app/todo/repository/TaskRepository.java
+++ src/main/java/com/app/todo/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.app.todo.repository;
 
-public class TaskRepository {
+import com.app.todo.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
---
+++ src/main/java/com/app/todo/services/TaskService.java
@@ -0,0 +1,22 @@
+package com.app.todo.services;
+
+
+import com.app.todo.models.Task;
+import com.app.todo.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskrepo;
+
+    public TaskService(TaskRepository taskrepo) {
+        this.taskrepo = taskrepo;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskrepo.findAll();
+    }
+}
